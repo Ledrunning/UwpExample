539fe60ac9a44c1e43b1a96eeadaa2adb0f0bb37
Ledrunning <ledrunner@hotmail.com>
2024-12-15 16:30:40 +0100

    Updated dialog service

--- Services/DialogService.cs
+++ Services/DialogService.cs
@@ -4,6 +4,7 @@ using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using UwpExample.Abstractions;
+using UwpExample.Controls.UserControls.View;
 
 namespace UwpExample.Services
 {
